ba513cd4f4f3f9cc35bc9a900e30c13465d5a159
ErykZenith <167299127+ErykZenith@users.noreply.github.com>
2024-06-28 17:34:03 +0700

    1.0.0

---
+++ sharing/web/script.js
@@ -0,0 +1,6 @@
+// Post('Url ของ Script ที่ต้องการ', 'ข้อมูลที่ต้องการส่ง')
+// GetWindowFromScriptName('ชื่อ Script ที่ต้องการ').addEventListener('message', (event) => {
+//     const { data } = event.data;
+// })
+
+console.log("ERYK ZENITH ");
